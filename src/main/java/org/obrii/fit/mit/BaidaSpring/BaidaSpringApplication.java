c8f961b0d688b8ee4553b3212f896429b486e92f
danila <danila@DESKTOP-R2V4DVE>
2021-04-15 14:56:41 +0300

    ex

---
+++ src/main/java/org/obrii/fit/mit/BaidaSpring/BaidaSpringApplication.java
@@ -0,0 +1,13 @@
+package org.obrii.fit.mit.BaidaSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaidaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaidaSpringApplication.class, args);
+	}
+
+}
